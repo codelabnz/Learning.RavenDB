cb88467c9ac47bde6192b1b2577d1ae0de9a8bf7
Simon Fox <simon.m.fox@gmail.com>
2015-02-26 17:07:37 +1300

    added projecting from fields stored in an index

--- Learning.RavenDB/RavenTransformerSuite.cs
+++ Learning.RavenDB/RavenTransformerSuite.cs
@@ -1,15 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NodaTime;
-using Raven.Tests.Helpers;
+using Raven.Client;
 using Raven.Client.NodaTime;
+using Raven.Tests.Helpers;
 using Xunit;
 
 namespace Learning.RavenDB
 {
+    public class ProjectQueryResultFromStoredFields : RavenTestBase
+    {
+        [Fact]
+        public void ProjectFromStoredFields()
+        {
+            var station = new Station
+            {
+                Code = "WKOMORE"
+            };
+
+            using (var documentStore = NewDocumentStore())
+            {
+                documentStore.ConfigureForNodaTime();
+                new SpotLines_ByFirstStationStored().Execute(documentStore);
+
+                using (var session = documentStore.OpenSession())
+                {
+                    session.Store(station);
+
+                    var spotLine = new ContractSpotLine
+                    {
+                        Month = new LocalDate(2015, 02, 01),
+                        StationDescription = "Temp Description - Store As Code In Index",
+                        StationIds = new[] { station.Id }
+                    };
+
+                    session.Store(spotLine);
+
+                    session.SaveChanges();
+                }
+
+                using (var session = documentStore.OpenSession())
+                {
+                    // transform ContractSpotLine results to StationViewModel by pulling the available fields
+                    // from equivalent index stored fields.
+                    var stations = session.Query<ContractSpotLine, SpotLines_ByFirstStationStored>()
+                                        .Customize(q => q.WaitForNonStaleResults(TimeSpan.FromSeconds(5)))
+                                        .ProjectFromIndexFieldsInto<SpotLines_ByFirstStationStored.StationViewModel>()
+                                        .ToList();
+
+                    Assert.Equal(stations.FirstOrDefault().StationCode, station.Code);
+                }
+            }
+        }
+    }
+
     public class TransformRavenQueryResult : RavenTestBase
     {
         [Fact]
---
+++ Learning.RavenDB/SpotLines_ByFirstStationStored.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace Learning.RavenDB
+{
+    public class SpotLines_ByFirstStationStored : AbstractIndexCreationTask<ContractSpotLine>
+    {
+        public class StationViewModel
+        {
+            public string StationCode { get; set; }
+        }
+
+        public SpotLines_ByFirstStationStored()
+        {
+            Map = spotLines => from line in spotLines
+                               from station in line.StationIds
+                               select new
+                               {
+                                   StationCode = LoadDocument<Station>(station).Code
+                               };
+
+
+            // this example is a bit dumb as Station Code isn't a property on the indexed doc (ContractSpotLine)
+            // but what this is doing is creating a property of StationCode in the index which means when transforming
+            // results of this index to type StationViewModel, the whole result object can be built from the index
+            // fields without having to go back to the indexed document
+            Store("StationCode", FieldStorage.Yes);
+        }
+    }
+}
